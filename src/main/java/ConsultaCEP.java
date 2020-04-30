93f30db777f6927d87e8e36030d91e7eb4596adc
Josinaldo Júnior <josinaldo_jr@hotmail.com>
2020-04-30 16:13:44 -0300

    teste inicial

--- src/main/java/ConsultaCEP.java
+++ src/main/java/ConsultaCEP.java
@@ -1,2 +1,21 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ConsultaCEP {
+
+
+
+  @Test
+  public void testValidarConsultaDeCEP() {
+     System.setProperty("webdriver.chrome.driver", "c:\\temp\\drivers\\chromedriver.exe");
+      WebDriver navegador = new ChromeDriver();
+      navegador.get("http://www.buscacep.correios.com.br/sistemas/buscacep/buscaCepEndereco.cfm");
+      navegador.findElement(By.name("relaxation")).click();
+      navegador.findElement(By.name("relaxation")).sendKeys("06445500");
+      navegador.findElement(By.cssSelector(".btn2")).click();
+
+  }
+
 }
